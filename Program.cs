b539d0838eb2b3a236c54eca489456209ae56d78
valenza.ka@gmail.com <valenza.ka@gmail.com>
2025-07-08 20:03:45 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestPerfomanceLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
